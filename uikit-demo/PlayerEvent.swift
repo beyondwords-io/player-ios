d5b5051790e27b024ed21e6f2043fbf90d256645
nklhtv <nklhtv@gmail.com>
2023-06-26 17:49:05 +0300

    PlayerEvent struct added

---
+++ uikit-demo/PlayerEvent.swift
@@ -0,0 +1,11 @@
+struct PlayerEvent: Codable {
+    var id: String
+    var type: String
+    var description: String
+    var initiatedBy: String
+    var emittedFrom: String
+    var status: String
+    var createdAt: String
+    var processedAt: String
+    // TODO var changedProps
+}
