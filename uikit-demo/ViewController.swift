fe545f8254634395aa53f5f414646997101ba4ed
nklhtv <nklhtv@gmail.com>
2023-07-06 13:25:00 +0300

    impl PlayerDelegate in the ViewController

--- uikit-demo/ViewController.swift
+++ uikit-demo/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PlayerDelegate {
     private weak var playerView: PlayerView!
     
     override func viewDidLoad() {
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
         playerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         playerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         playerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        playerView.delegate = self
         playerView.load(PlayerSettings(projectId: 19713, contentId: "52a12d13-3ed0-48b0-9f82-12177d4a8ca1"))
         self.playerView = playerView
+    }
+    
+    func player(_ player: PlayerView, onEvent event: PlayerEvent, settings: PlayerSettings) {
+        print("onEvent \(event) \(settings)")
     }
 }
