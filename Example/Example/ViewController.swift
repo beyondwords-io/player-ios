060ee20105a9f5c48131ac319c9947eb16580f09
nklhtv <nklhtv@gmail.com>
2023-08-14 18:27:42 +0300

    added support for player style

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,162 +7,167 @@
 
 import UIKit
 import BeyondWordsPlayer
+import JVFloatLabeledText
 
 class ViewController: UIViewController {
     
-    private weak var projectIdField: UITextField!
-    private weak var contentIdField: UITextField!
-    private weak var sourceIdField: UITextField!
-    private weak var playlistIdField: UITextField!
-    private weak var customUICheckbox: UISwitch!
+    private weak var projectIdInput: UITextView!
+    private weak var contentIdInput: UITextView!
+    private weak var sourceIdInput: UITextView!
+    private weak var playlistIdInput: UITextView!
+    private weak var playerStyleInput: UITextView!
+    private weak var playerUIInput: UITextView!
     private weak var playerViewContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let settingsView = UIStackView()
-        settingsView.axis = .vertical
-        settingsView.spacing = 16
-        settingsView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(settingsView)
-        settingsView.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
-        settingsView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        settingsView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        let projectIdInput = createTextInputField(name: "projectId", placeholder: "Project id")
+        projectIdInput.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
         
-        let projectIdContainer = UIStackView()
-        projectIdContainer.axis = .horizontal
-        projectIdContainer.distribution = .fill
-        projectIdContainer.spacing = 8
-        projectIdContainer.translatesAutoresizingMaskIntoConstraints = false
-        let projectIdLabel = UILabel()
-        projectIdLabel.text = "Project id:"
-        projectIdLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        projectIdLabel.translatesAutoresizingMaskIntoConstraints = false
-        projectIdContainer.addArrangedSubview(projectIdLabel)
-        let projectIdField = UITextField()
-        projectIdField.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
-        projectIdField.placeholder = "Enter project id"
-        projectIdField.translatesAutoresizingMaskIntoConstraints = false
-        projectIdContainer.addArrangedSubview(projectIdField)
-        settingsView.addArrangedSubview(projectIdContainer)
+        let contentIdInput = createTextInputField(name: "contentId", placeholder: "Content id:")
+        contentIdInput.topAnchor.constraint(equalTo: projectIdInput.bottomAnchor, constant: 12).isActive = true
         
-        let contentIdContainer = UIStackView()
-        contentIdContainer.axis = .horizontal
-        contentIdContainer.distribution = .fill
-        contentIdContainer.spacing = 8
-        contentIdContainer.translatesAutoresizingMaskIntoConstraints = false
-        let contentIdLabel = UILabel()
-        contentIdLabel.text = "Content id:"
-        contentIdLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        contentIdLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentIdContainer.addArrangedSubview(contentIdLabel)
-        let contentIdField = UITextField()
-        contentIdField.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
-        contentIdField.placeholder = "Enter content id"
-        contentIdField.translatesAutoresizingMaskIntoConstraints = false
-        contentIdContainer.addArrangedSubview(contentIdField)
-        settingsView.addArrangedSubview(contentIdContainer)
+        let sourceIdInput = createTextInputField(name: "sourceId", placeholder: "Source id:")
+        sourceIdInput.topAnchor.constraint(equalTo: contentIdInput.bottomAnchor, constant: 12).isActive = true
         
-        let sourceIdContainer = UIStackView()
-        sourceIdContainer.axis = .horizontal
-        sourceIdContainer.distribution = .fill
-        sourceIdContainer.spacing = 8
-        sourceIdContainer.translatesAutoresizingMaskIntoConstraints = false
-        let sourceIdLabel = UILabel()
-        sourceIdLabel.text = "Source id:"
-        sourceIdLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        sourceIdLabel.translatesAutoresizingMaskIntoConstraints = false
-        sourceIdContainer.addArrangedSubview(sourceIdLabel)
-        let sourceIdField = UITextField()
-        sourceIdField.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
-        sourceIdField.placeholder = "Enter source id"
-        sourceIdField.translatesAutoresizingMaskIntoConstraints = false
-        sourceIdContainer.addArrangedSubview(sourceIdField)
-        settingsView.addArrangedSubview(sourceIdContainer)
+        let playlistIdInput = createTextInputField(name: "playlistId", placeholder: "Playlist id:")
+        playlistIdInput.topAnchor.constraint(equalTo: sourceIdInput.bottomAnchor, constant: 12).isActive = true
         
-        let playlistIdContainer = UIStackView()
-        playlistIdContainer.axis = .horizontal
-        playlistIdContainer.distribution = .fill
-        playlistIdContainer.spacing = 8
-        playlistIdContainer.translatesAutoresizingMaskIntoConstraints = false
-        let playlistIdLabel = UILabel()
-        playlistIdLabel.text = "Playlist id:"
-        playlistIdLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        playlistIdLabel.translatesAutoresizingMaskIntoConstraints = false
-        playlistIdContainer.addArrangedSubview(playlistIdLabel)
-        let playlistIdField = UITextField()
-        playlistIdField.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
-        playlistIdField.placeholder = "Enter playlist id"
-        playlistIdField.translatesAutoresizingMaskIntoConstraints = false
-        playlistIdContainer.addArrangedSubview(playlistIdField)
-        settingsView.addArrangedSubview(playlistIdContainer)
+        let playerStyleInput = createSelectInputField(name: "playerStyle", placeholder: "Player style:", action: #selector(selectPlayerStyle))
+        playerStyleInput.topAnchor.constraint(equalTo: playlistIdInput.bottomAnchor, constant: 12).isActive = true
         
-        let customUIContainer = UIStackView()
-        customUIContainer.axis = .horizontal
-        customUIContainer.distribution = .fill
-        customUIContainer.spacing = 8
-        customUIContainer.translatesAutoresizingMaskIntoConstraints = false
-        let customUILabel = UILabel()
-        customUILabel.text = "Custom UI:"
-        customUILabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        customUILabel.translatesAutoresizingMaskIntoConstraints = false
-        customUIContainer.addArrangedSubview(customUILabel)
-        let customUICheckbox = UISwitch()
-        customUICheckbox.translatesAutoresizingMaskIntoConstraints = false
-        customUIContainer.addArrangedSubview(customUICheckbox)
-        settingsView.addArrangedSubview(customUIContainer)
+        let playerUIInput = createSelectInputField(name: "playerUI", placeholder: "Player UI:", action: #selector(selectPlayerUI))
+        playerUIInput.topAnchor.constraint(equalTo: playerStyleInput.bottomAnchor, constant: 12).isActive = true
         
-        let loadButton = UIButton(type: .system)
-        loadButton.setTitle("Load", for: .normal)
-        loadButton.setTitleColor(.black, for: .normal)
-        loadButton.translatesAutoresizingMaskIntoConstraints = false
-        settingsView.addArrangedSubview(loadButton)
+        let loadButton = createButton(title: "Load Player", action: #selector(load))
+        loadButton.topAnchor.constraint(equalTo: playerUIInput.bottomAnchor, constant: 12).isActive = true
         
         let playerViewContainer = UIView()
         playerViewContainer.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(playerViewContainer)
-        playerViewContainer.topAnchor.constraint(equalTo: settingsView.bottomAnchor).isActive = true
+        playerViewContainer.topAnchor.constraint(equalTo: loadButton.bottomAnchor, constant: 12).isActive = true
         playerViewContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         playerViewContainer.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         playerViewContainer.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
-        projectIdField.text = UserDefaults.standard.string(forKey: "projectId")
-        contentIdField.text = UserDefaults.standard.string(forKey: "contentId")
-        playlistIdField.text = UserDefaults.standard.string(forKey: "playlistId")
-        sourceIdField.text = UserDefaults.standard.string(forKey: "sourceId")
-        customUICheckbox.setOn(UserDefaults.standard.bool(forKey: "customUI"), animated: false)
-        
-        self.projectIdField = projectIdField
-        self.contentIdField = contentIdField
-        self.playlistIdField = playlistIdField
-        self.sourceIdField = sourceIdField
-        self.customUICheckbox = customUICheckbox
+        self.projectIdInput = projectIdInput
+        self.contentIdInput = contentIdInput
+        self.sourceIdInput = sourceIdInput
+        self.playlistIdInput = playlistIdInput
+        self.playerStyleInput = playerStyleInput
+        self.playerUIInput = playerUIInput
         self.playerViewContainer = playerViewContainer
+    }
+    
+    func createButton(title: String, action: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 4
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        button.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        button.addTarget(self, action: action, for: .touchUpInside)
+        return button
+    }
+    
+    func createSelectInputField(name: String, placeholder: String, action: Selector) -> UITextView {
+        let inputField = createTextInputField(name: name, placeholder: placeholder)
+        inputField.isEditable = false
+        inputField.isSelectable = false
         
-        loadButton.addTarget(self, action: #selector(load), for: .touchUpInside)
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: action)
+        tapRecognizer.numberOfTapsRequired = 1
+        inputField.addGestureRecognizer(tapRecognizer)
+        
+        return inputField
+    }
+    
+    func createTextInputField(name: String, placeholder: String) -> UITextView {
+        let inputField = JVFloatLabeledTextView()
+        inputField.text = UserDefaults.standard.string(forKey: name)
+        inputField.translatesAutoresizingMaskIntoConstraints = false
+        inputField.placeholder = "  " + placeholder
+        inputField.isScrollEnabled = false
+        inputField.sizeToFit()
+        inputField.autocorrectionType = .no
+        inputField.spellCheckingType = .no
+        inputField.textContainerInset = .init(top: 0, left: 6, bottom: 0, right: 6)
+        view.addSubview(inputField)
+        inputField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        inputField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        inputField.heightAnchor.constraint(equalToConstant: 56).isActive = true
+        
+        let borderBottom = UIView()
+        borderBottom.backgroundColor = .lightGray
+        borderBottom.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(borderBottom)
+        borderBottom.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        borderBottom.bottomAnchor.constraint(equalTo: inputField.bottomAnchor).isActive = true
+        borderBottom.leftAnchor.constraint(equalTo: inputField.leftAnchor).isActive = true
+        borderBottom.rightAnchor.constraint(equalTo: inputField.rightAnchor).isActive = true
+        
+        return inputField
+    }
+    
+    @objc func selectPlayerStyle() {
+        let alert = UIAlertController(title: "Player style", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Small", style: .default , handler: { (UIAlertAction) in
+            self.playerStyleInput.text = "small"
+        }))
+        alert.addAction(UIAlertAction(title: "Standard", style: .default , handler: { (UIAlertAction) in
+            self.playerStyleInput.text = "standard"
+        }))
+        alert.addAction(UIAlertAction(title: "Large", style: .default , handler: { (UIAlertAction) in
+            self.playerStyleInput.text = "large"
+        }))
+        alert.addAction(UIAlertAction(title: "Video", style: .default , handler: { (UIAlertAction) in
+            self.playerStyleInput.text = "video"
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    @objc func selectPlayerUI() {
+        let alert = UIAlertController(title: "Player UI", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Default", style: .default , handler: { (UIAlertAction) in
+            self.playerUIInput.text = "default"
+        }))
+        alert.addAction(UIAlertAction(title: "Custom", style: .default , handler: { (UIAlertAction) in
+            self.playerUIInput.text = "custom"
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
     @objc func load() {
-        UserDefaults.standard.setValue(projectIdField.text, forKey: "projectId")
-        UserDefaults.standard.setValue(contentIdField.text, forKey: "contentId")
-        UserDefaults.standard.setValue(playlistIdField.text, forKey: "playlistId")
-        UserDefaults.standard.setValue(sourceIdField.text, forKey: "sourceId")
-        UserDefaults.standard.setValue(customUICheckbox.isOn, forKey: "customUI")
+        UserDefaults.standard.setValue(projectIdInput.text, forKey: "projectId")
+        UserDefaults.standard.setValue(contentIdInput.text, forKey: "contentId")
+        UserDefaults.standard.setValue(sourceIdInput.text, forKey: "sourceId")
+        UserDefaults.standard.setValue(playlistIdInput.text, forKey: "playlistId")
+        UserDefaults.standard.setValue(playerStyleInput.text, forKey: "playerStyle")
+        UserDefaults.standard.setValue(playerUIInput.text, forKey: "playerUI")
+
         let playerSettings = PlayerSettings(
-            projectId: Int(projectIdField.text ?? ""),
-            contentId: contentIdField.text,
-            playlistId: Int(playlistIdField.text ?? ""),
-            sourceId: sourceIdField.text
+            projectId: !projectIdInput.text.isEmpty ? Int(projectIdInput.text) : nil,
+            contentId: !contentIdInput.text.isEmpty ? contentIdInput.text : nil,
+            playlistId: !playlistIdInput.text.isEmpty ? Int(playlistIdInput.text) : nil,
+            sourceId: !sourceIdInput.text.isEmpty ? sourceIdInput.text : nil,
+            playerStyle: !playerStyleInput.text.isEmpty ? playerStyleInput.text : nil
         )
         
         let playerView: UIView
-        switch customUICheckbox.isOn {
-        case true:
+        switch playerUIInput.text {
+        case "custom":
             let customPlayerView = CustomPlayerView()
             customPlayerView.verbose = true
             customPlayerView.load(playerSettings)
             playerView = customPlayerView
-        case false:
+        default:
             let defaultPlayerView = PlayerView()
             defaultPlayerView.verbose = true
             defaultPlayerView.load(playerSettings)
