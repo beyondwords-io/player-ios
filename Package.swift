97cc6be0ab4fcc72ee6ad92f36c534b0e921aa51
Nikola Hristov <nklhtv@gmail.com>
2025-09-03 16:28:06 +0300

    SPM support (#7)
    * Add SPM support
    
    * downgrade swift-tools-version to 5.3
    
    * remove whitespace after swift tools specifier
    
    * fix loading `player.html` for SPM

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "BeyondWordsPlayer",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BeyondWordsPlayer",
+            targets: ["BeyondWordsPlayer"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "BeyondWordsPlayer",
+            path: "BeyondWordsPlayer/BeyondWordsPlayer",
+            resources: [
+                .process("player.html")
+            ]
+        )
+    ]
+)
