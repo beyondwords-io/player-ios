e8525b0b345df3018a4223052e1a27c2bb9a4860
nklhtv <nklhtv@gmail.com>
2026-05-26 18:54:04 +0300

    add support for `outroPlaybackMode`

--- BeyondWordsPlayer/BeyondWordsPlayer/PlayerSettings.swift
+++ BeyondWordsPlayer/BeyondWordsPlayer/PlayerSettings.swift
@@ -32,6 +32,7 @@ public struct PlayerSettings: Codable {
     public var contentIndex: Int? = nil
     public var introsOutros: [IntroOutro]? = nil
     public var introsOutrosIndex: Int? = nil
+    public var outroPlaybackMode: String? = nil
     public var adverts: [Advert]? = nil
     public var advertIndex: Int? = nil
     public var minDurationForMidroll: Float? = nil
@@ -207,7 +208,7 @@ public struct PlayerSettings: Codable {
         }
     }
     
-    public init(playerApiUrl: String? = nil, projectId: Int? = nil, contentId: String? = nil, playlistId: Int? = nil, sourceId: String? = nil, sourceUrl: String? = nil, accessTier: String? = nil, segmentLimit: Int? = nil, playlist: [Identifier]? = nil, summary: Bool? = nil, loadContentAs: [String]? = nil, contentVariant: String? = nil, videoSizes: [String]? = nil, continuousPlaybackMode: String? = nil, showUserInterface: Bool? = nil, playerStyle: String? = nil, playerTitle: String? = nil, callToAction: String? = nil, skipButtonStyle: String? = nil, playlistStyle: String? = nil, playlistToggle: String? = nil, mediaSession: String? = nil, content: [Content]? = nil, contentIndex: Int? = nil, introsOutros: [IntroOutro]? = nil, introsOutrosIndex: Int? = nil, adverts: [Advert]? = nil, advertIndex: Int? = nil, minDurationForMidroll: Float? = nil, minTimeUntilEndForMidroll: Float? = nil, persistentAdImage: Bool? = nil, persistentIndex: Int? = nil, duration: Float? = nil, currentTime: Float? = nil, playbackState: String? = nil, playbackRate: Float? = nil, playbackRates: [Float]? = nil, textColor: String? = nil, backgroundColor: String? = nil, iconColor: String? = nil, logoIconEnabled: Bool? = nil, currentSegment: Segment? = nil, widgetPosition: String? = nil, widgetStyle: String? = nil, loadedMedia: Media? = nil, advertConsent: String? = nil, analyticsConsent: String? = nil, analyticsCustomUrl: String? = nil, analyticsTag: String? = nil, mediaCustomUrl: String? = nil, bundleIdentifier: String? = nil, vendorIdentifier: String? = nil) {
+    public init(playerApiUrl: String? = nil, projectId: Int? = nil, contentId: String? = nil, playlistId: Int? = nil, sourceId: String? = nil, sourceUrl: String? = nil, accessTier: String? = nil, segmentLimit: Int? = nil, playlist: [Identifier]? = nil, summary: Bool? = nil, loadContentAs: [String]? = nil, contentVariant: String? = nil, videoSizes: [String]? = nil, continuousPlaybackMode: String? = nil, showUserInterface: Bool? = nil, playerStyle: String? = nil, playerTitle: String? = nil, callToAction: String? = nil, skipButtonStyle: String? = nil, playlistStyle: String? = nil, playlistToggle: String? = nil, mediaSession: String? = nil, content: [Content]? = nil, contentIndex: Int? = nil, introsOutros: [IntroOutro]? = nil, introsOutrosIndex: Int? = nil, outroPlaybackMode: String? = nil, adverts: [Advert]? = nil, advertIndex: Int? = nil, minDurationForMidroll: Float? = nil, minTimeUntilEndForMidroll: Float? = nil, persistentAdImage: Bool? = nil, persistentIndex: Int? = nil, duration: Float? = nil, currentTime: Float? = nil, playbackState: String? = nil, playbackRate: Float? = nil, playbackRates: [Float]? = nil, textColor: String? = nil, backgroundColor: String? = nil, iconColor: String? = nil, logoIconEnabled: Bool? = nil, currentSegment: Segment? = nil, widgetPosition: String? = nil, widgetStyle: String? = nil, loadedMedia: Media? = nil, advertConsent: String? = nil, analyticsConsent: String? = nil, analyticsCustomUrl: String? = nil, analyticsTag: String? = nil, mediaCustomUrl: String? = nil, bundleIdentifier: String? = nil, vendorIdentifier: String? = nil) {
         self.playerApiUrl = playerApiUrl
         self.projectId = projectId
         self.contentId = contentId
@@ -234,6 +235,7 @@ public struct PlayerSettings: Codable {
         self.contentIndex = contentIndex
         self.introsOutros = introsOutros
         self.introsOutrosIndex = introsOutrosIndex
+        self.outroPlaybackMode = outroPlaybackMode
         self.adverts = adverts
         self.advertIndex = advertIndex
         self.minDurationForMidroll = minDurationForMidroll
